482402c7f43edea914e9cb3aafabece25d3a6429
roytal <roytt0@gmail.com>
2023-11-03 21:49:57 +0200

    application wrap
    * adding openapi docs and swagger lib
    * sever start only when data is hot

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 require('dotenv').config({path: '../.env'});
 const express = require("express");
+const swaggerUI = require("swagger-ui-express");
 const cors = require("cors");
 const apiRouter = require("./routes/api");
 const {systemLogger} = require("../utils/logger");
 const {fetchMoviesCasts} = require("../utils/movies");
+const {docs} = require('../docs/openapi')
 const app = express();
 
 app.use(express.json({limit: "100mb"}));
 app.use(cors());
 
-// Security - api response headers
+// security - api response headers
 app.use(function (req, res, next) {
     res.setHeader("X-Powered-By", "PHP");
     res.setHeader(
@@ -19,15 +21,30 @@ app.use(function (req, res, next) {
     next();
 });
 
-// Normal express config defaults
+// normal express config defaults
 app.use(express.urlencoded({extended: true}));
 
 // routes
 app.use("/", apiRouter);
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(docs));
 
-// start the server
-app.listen(process.env.PORT || 3000, async () => {
-    await fetchMoviesCasts()
-    console.log(`server running port ${process.env.PORT}`);
-    systemLogger.info(`server start, port:${process.env.PORT}`);
-});
+// fetch all data to local storage and start the server
+const startServer = async () => {
+    try {
+        await fetchMoviesCasts();
+        systemLogger.info(`fetched all movie calls to local storage`);
+        console.log(`fetched all movie calls to local storage`);
+
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+            systemLogger.info(`Server started, port: ${port}`);
+        });
+    } catch (error) {
+        console.error('Error fetching movie casts:', error);
+        systemLogger.error('Error fetching movie casts:', error);
+        process.exit(1);
+    }
+};
+
+startServer();
